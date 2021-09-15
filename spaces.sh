3045c9551ee750d53e886cd299d1d485a6837e9c
xy1sul41n <74555507+xy1sul41n@users.noreply.github.com>
2021-09-15 21:29:04 +0200

    Update spaces.sh

--- spaces.sh
+++ spaces.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#Eliminar los espacios de un texto
 
 echo "Introduce el texto para eliminar los espacios"
 read text
-res="$(echo -e $text | tr -d '[[:space:]]'
+res="$(echo -e $text | tr -d '[[:space:]]' 
+
+echo "Los puertos se han copiado a la clipboard\n\n"
 echo $res
 
